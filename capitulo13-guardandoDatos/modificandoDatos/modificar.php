7cfb4391992e311546ab3409b9400f17749624be
jofuto29 <89996057+jofuto29@users.noreply.github.com>
2023-02-20 08:21:57 +0100

    Update modificar.php

--- capitulo13-guardandoDatos/modificandoDatos/modificar.php
+++ capitulo13-guardandoDatos/modificandoDatos/modificar.php
@@ -29,4 +29,19 @@ if(isset($_GET["codigo"])and $_GET["codigo"]<>""){
 }
 
 
+/*
+<?php
+$username = "tu_usuario";
+$password = "tu_contraseña";
+$database = "tu_basededatos";
+$mysqli = new mysqli("localhost", $username, $password, $database);
+$query="CREATE TABLE tablename(id int(6) NOT NULL auto_increment,first varchar(15) NOT NULL,last varchar(15) NOT NULL,field1-name varchar(20) NOT NULL,field2-name varchar(20)NOT NULL,field3-name varchar(20) NOT NULL,field4-name varchar(30) NOT NULL, field5-name varchar(30)NOT NULL,PRIMARY KEY (id),UNIQUE id (id),KEY id_2 (id))";
+$mysqli->query("$query");
+$mysqli->close();
+?>
+
+
+*/
+
+
 ?>
